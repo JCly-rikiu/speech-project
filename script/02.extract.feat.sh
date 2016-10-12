8b66a53bd37e71d8292a91564f81dc49e87c1923
JCly-rikiu <jk123456789jp@gmail.com>
2016-10-13 01:37:50 +0800

    first commit

---
+++ script/02.extract.feat.sh
@@ -0,0 +1,57 @@
+
+if [ -f setup.sh ]; then
+  . setup.sh;
+else
+  echo "ERROR: setup.sh is missing!";
+  exit 1;
+fi
+
+cmvn_dir=exp/cmvn
+
+path=feat
+options="--use-energy=false"
+
+echo "Acoustic features will be extracted in the following directory : "
+echo "  $path"
+
+mkdir -p $path
+mkdir -p $cmvn_dir
+
+echo "Extracting train set"
+target=train
+log=$path/$target.extract.log
+compute-mfcc-feats --verbose=2 $options scp:material/$target.wav.scp ark,t,scp:$path/$target.13.ark,$path/$target.13.scp 2> $log
+# TODO:
+#	1. use add-deltas to add delta features to original mfcc feature set
+#	2. use compute-cmvn-stats & apply-cmvn to compute cmvn statistics and apply cmvn with global means and variatnces to previous 39-dim mfcc feature set
+add-deltas ark:$path/$target.13.ark ark,t,scp:$path/$target.feat.ark,$path/$target.feat.scp 2> $log
+compute-cmvn-stats scp:$path/$target.feat.scp ark,t,scp:$path/$target.cmvn.ark,$path/$target.cmvn.scp 2> $log
+apply-cmvn scp:$path/$target.cmvn.scp scp:$path/$target.feat.scp ark:$path/$target.39.cmvn.ark 2> $log
+
+echo "Extracting dev set"
+target=dev
+log=$path/$target.extract.log
+compute-mfcc-feats --verbose=2 $options scp:material/$target.wav.scp ark,t,scp:$path/$target.13.ark,$path/$target.13.scp 2> $log
+# TODO:
+#	1. use add-deltas to add delta features to original mfcc feature set
+#	2. use compute-cmvn-stats & apply-cmvn to compute cmvn statistics and apply cmvn with global means and variatnces to previous 39-dim mfcc feature set
+add-deltas ark:$path/$target.13.ark ark,t,scp:$path/$target.feat.ark,$path/$target.feat.scp 2> $log
+compute-cmvn-stats scp:$path/$target.feat.scp ark,t,scp:$path/$target.cmvn.ark,$path/$target.cmvn.scp 2> $log
+apply-cmvn scp:$path/$target.cmvn.scp scp:$path/$target.feat.scp ark:$path/$target.39.cmvn.ark 2> $log
+
+echo "Extracting test set"
+target=test
+log=$path/$target.extract.log
+compute-mfcc-feats --verbose=2 $options scp:material/$target.wav.scp ark,t,scp:$path/$target.13.ark,$path/$target.13.scp 2> $log
+# TODO:
+#	1. use add-deltas to add delta features to original mfcc feature set
+#	2. use compute-cmvn-stats & apply-cmvn to compute cmvn statistics and apply cmvn with global means and variatnces to previous 39-dim mfcc feature set
+add-deltas ark:$path/$target.13.ark ark,t,scp:$path/$target.feat.ark,$path/$target.feat.scp 2> $log
+compute-cmvn-stats scp:$path/$target.feat.scp ark,t,scp:$path/$target.cmvn.ark,$path/$target.cmvn.scp 2> $log
+apply-cmvn scp:$path/$target.cmvn.scp scp:$path/$target.feat.scp ark:$path/$target.39.cmvn.ark 2> $log
+
+sec=$SECONDS
+
+echo ""
+echo "Execution time for whole script = `utility/timer.pl $sec`"
+echo ""
